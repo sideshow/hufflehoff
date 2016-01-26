7565458e4253450b5aeaa8159f28761f24ab02cd
Adam Jones <adam@digirati.co.nz>
2016-01-26 23:22:06 +1300

    Update Mixfile

--- mix.exs
+++ mix.exs
@@ -7,26 +7,28 @@ defmodule Hufflehoff.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     description: description,
+     package: package,
+     deps: deps,]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    A Huffman encoder/decoder for HTTP/2 headers.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Adam Jones"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/sideshow/hufflehoff"}]
   end
 end
